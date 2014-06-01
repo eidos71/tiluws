a422f04d0bd9bc080ab7fe33371fff2109475e7b
eidos71 <eidos33@gmail.com>
2014-06-01 13:51:34 +0200

    Readying Spring-ws ready by annotation no xml config.

--- src/main/java/tiluws/config/ApplicationContext.java
+++ src/main/java/tiluws/config/ApplicationContext.java
@@ -1,17 +1,27 @@
 package tiluws.config;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.commons.CommonsXsdSchemaCollection;
 
 @Configuration
 @ComponentScan(basePackages = {"tiluws.service","tiluws.endpoint"})
@@ -32,5 +42,35 @@ public class ApplicationContext {
         viewResolver.setSuffix(VIEW_RESOLVER_SUFFIX);
 
         return viewResolver;
-    }    
+
+    } 
+
+    @Bean
+    @Autowired
+    /**
+     * 
+     * @param resource
+     * @return
+     */
+   public DefaultWsdl11Definition accountDetailsService() throws IOException{
+    	DefaultWsdl11Definition accountDetailsService = new DefaultWsdl11Definition();
+    	CommonsXsdSchemaCollection schemaCollection = new CommonsXsdSchemaCollection();
+    	schemaCollection.setInline(true);
+    	org.springframework.core.io.ClassPathResource resource = 
+    			new ClassPathResource("schemas/AccountDetailsServiceOperations.xsd");
+    	//resource.createRelative;
+    	Collection<org.springframework.core.io.Resource> resourceList=
+    			new LinkedList<org.springframework.core.io.Resource>();
+    	resourceList.add(resource);
+    	schemaCollection.setXsds(resourceList.toArray(new org.springframework.core.io.ClassPathResource[resourceList.size()]  ));
+    	accountDetailsService.setSchemaCollection(schemaCollection);
+    	accountDetailsService.setPortTypeName("AccountDetailsService");
+    	accountDetailsService.setServiceName("AccountDetailsService");     	//AccountDetailsServices??
+    	accountDetailsService.setTargetNamespace("http://webservices.tilufwlow/accountservice");
+    	accountDetailsService.setLocationUri("/endpoints/");
+   
+
+    	
+	   return accountDetailsService;
+   }
 }
--- src/main/java/tiluws/config/TiluWsInitializer.java
+++ src/main/java/tiluws/config/TiluWsInitializer.java
@@ -6,10 +6,12 @@ import javax.servlet.ServletRegistration;
 
 
 
+
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
 
 /**
  * Uses the standard WS Initializer
@@ -20,16 +22,22 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class TiluWsInitializer implements WebApplicationInitializer {
 
     private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
-    private static final String DISPATCHER_SERVLET_MAPPING = "/";
-    
+    private static final String DISPATCHER_SERVLET_MAPPING = "/mvc/*";
+    private static final String DISPATCHER_SERVLET_WS="spring-ws";
+    private static final String DISPATCHER_SERVLET_WS_MAPPING="/webservices/*";
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
         rootContext.register(ApplicationContext.class);
 
+        ServletRegistration.Dynamic ws_dispatcher=
+        		servletContext.addServlet(DISPATCHER_SERVLET_WS, new MessageDispatcherServlet(rootContext));
+        ws_dispatcher.setLoadOnStartup(1); 
+        ws_dispatcher.addMapping(DISPATCHER_SERVLET_WS_MAPPING);
+        ws_dispatcher.setInitParameter("transformWsdlLocations", "true");
         ServletRegistration.Dynamic dispatcher = 
         		servletContext.addServlet(DISPATCHER_SERVLET_NAME, new DispatcherServlet(rootContext));
-        dispatcher.setLoadOnStartup(1);
+        dispatcher.setLoadOnStartup(2);
         dispatcher.addMapping(DISPATCHER_SERVLET_MAPPING);
 
         servletContext.addListener(new ContextLoaderListener(rootContext));
